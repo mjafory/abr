a93941b2c2576d362dfb4076f344b54eca41caf6
Jafory-Software <jafory-software@Jafory-Softwares-MacBook-Pro.local>
2022-03-06 23:39:23 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+const video = document.querySelector("video")
+const textElem = document.querySelector("[data-text]")
+
+async function setup() {
+  const stream = await navigator.mediaDevices.getUserMedia({ video: true })
+  video.srcObject = stream
+
+  video.addEventListener("playing", async () => {
+    const worker = Tesseract.createWorker()
+    await worker.load()
+    await worker.loadLanguage("eng")
+    await worker.initialize("eng")
+
+    const canvas = document.createElement("canvas")
+    canvas.width = video.width
+    canvas.height = video.height
+
+    //Enter space for capturing data text
+    document.addEventListener("keypress", async e => {
+      if (e.code !== "Space") return
+      canvas.getContext("2d").drawImage(video, 0, 0, video.width, video.height)
+      const {
+        data: { text },
+      } = await worker.recognize(canvas)
+      speechSynthesis.speak(
+        new SpeechSynthesisUtterance(text.replace(/\s/g, " "))
+      )
+      textElem.textContent = text
+
+    })
+    
+    //Click button for capturing data text
+    const element = document.getElementById("click_button")
+    element.addEventListener("click",  async function(){
+    canvas.getContext("2d").drawImage(video, 0, 0, video.width, video.height)
+      const {
+        data: { text },
+      } = await worker.recognize(canvas)
+
+      speechSynthesis.speak(
+        new SpeechSynthesisUtterance(text.replace(/\s/g, " "))
+      )
+      textElem.textContent = text
+        
+    })
+  })
+}
+
+setup()
